8121499a2c2d0a287fe22643f41bcc9a2901be9f
Pilad <piladt@gmail.com>
2021-01-14 18:53:05 +0600

    Remove fluid pattern for every fluid
    Remove the registration of the fluid pattern for every fluid

--- src/main/scala/extracells/item/ItemFluidPattern.java
+++ src/main/scala/extracells/item/ItemFluidPattern.java
@@ -2,10 +2,8 @@ package extracells.item;
 
 import extracells.registries.ItemEnum;
 import net.minecraft.client.renderer.texture.IIconRegister;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.IIcon;
@@ -14,8 +12,6 @@ import net.minecraft.world.World;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
-
-import java.util.List;
 
 public class ItemFluidPattern extends ItemECBase {
 
@@ -71,18 +67,6 @@ public class ItemFluidPattern extends ItemECBase {
 	@Override
 	public int getSpriteNumber() {
 		return 1;
-	}
-
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	@Override
-	public void getSubItems(Item item, CreativeTabs creativeTab, List itemList) {
-		for (Fluid fluid : FluidRegistry.getRegisteredFluidIDsByFluid().keySet()) {
-			String name = "";
-			ItemStack itemStack = new ItemStack(this, 1);
-			itemStack.setTagCompound(new NBTTagCompound());
-			itemStack.getTagCompound().setString("fluidID", fluid.getName());
-			itemList.add(itemStack);
-		}
 	}
 
 	@Override
